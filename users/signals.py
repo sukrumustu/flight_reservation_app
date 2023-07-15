a6cf1c25a31667af5b4919130ce9787df8fada9d
sukru mustu <smustu1981@gmail.com>
2023-07-15 15:37:13 +0300

    new commit

--- users/signals.py
+++ users/signals.py
@@ -4,6 +4,8 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 
+#! bu signali bir user oluşturulduktan (register edildikten hemen) sonra Token da oluşturması yani otomatik login olması için kullandık. Bana user bilgisi ile birlikte Token'ın da dönmesi için views.py'da RegisterApi'nin create metodunu override ettik.
+
 @receiver(post_save, sender=User)
 def create_Token(sender, instance=None, created=False, **kwargs):
     if created:
